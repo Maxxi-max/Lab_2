3fd936bc55fd5ac61c037eb9118368a580c222af
Слушатель <@CBRMINIPC-DT002>
2025-11-21 11:29:19 +0300

    Отредактирован интерфейс программы, подключены расчеты.

--- Lab_2/Sem/Form1.Designer.cs
+++ Lab_2/Sem/Form1.Designer.cs
@@ -29,19 +29,44 @@
         private void InitializeComponent()
         {
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
             pictureBox1 = new PictureBox();
-            button7 = new Button();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            label11 = new Label();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(12, 12);
+            button1.Location = new Point(685, 274);
             button1.Name = "button1";
             button1.Size = new Size(103, 64);
             button1.TabIndex = 0;
@@ -49,102 +74,356 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += btn_type_click;
             // 
-            // button2
-            // 
-            button2.Location = new Point(124, 12);
-            button2.Name = "button2";
-            button2.Size = new Size(103, 64);
-            button2.TabIndex = 1;
-            button2.Text = "С натягом";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += btn_type_click;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(236, 12);
-            button3.Name = "button3";
-            button3.Size = new Size(103, 64);
-            button3.TabIndex = 2;
-            button3.Text = "Переходная 1";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += btn_type_click;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(572, 12);
-            button4.Name = "button4";
-            button4.Size = new Size(103, 64);
-            button4.TabIndex = 5;
-            button4.Text = "Переходная 4";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += btn_type_click;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(460, 12);
-            button5.Name = "button5";
-            button5.Size = new Size(103, 64);
-            button5.TabIndex = 4;
-            button5.Text = "Переходная 3";
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += btn_type_click;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(348, 12);
-            button6.Name = "button6";
-            button6.Size = new Size(103, 64);
-            button6.TabIndex = 3;
-            button6.Text = "Переходная 2";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += btn_type_click;
-            // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(22, 125);
+            pictureBox1.Location = new Point(12, 274);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(653, 311);
             pictureBox1.TabIndex = 6;
             pictureBox1.TabStop = false;
             // 
-            // button7
+            // textBox1
             // 
-            button7.Location = new Point(12, 82);
-            button7.Name = "button7";
-            button7.Size = new Size(103, 37);
-            button7.TabIndex = 7;
-            button7.Text = "ES+ EI+";
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click;
+            textBox1.Location = new Point(36, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(70, 23);
+            textBox1.TabIndex = 7;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(126, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(52, 139);
+            listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(184, 12);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(45, 139);
+            listBox2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 157);
+            button2.Name = "button2";
+            button2.Size = new Size(223, 40);
+            button2.TabIndex = 10;
+            button2.Text = "Рассчитать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btn_calculation;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(267, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(64, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(359, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(64, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(359, 44);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(64, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(267, 44);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(64, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(242, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 15);
+            label1.TabIndex = 15;
+            label1.Text = "ES";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(337, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(16, 15);
+            label2.TabIndex = 16;
+            label2.Text = "EI";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(243, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 15);
+            label3.TabIndex = 17;
+            label3.Text = "es";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(337, 47);
+            label4.Name = "label4";
+            label4.Size = new Size(16, 15);
+            label4.TabIndex = 18;
+            label4.Text = "ei";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(601, 18);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 15);
+            label5.TabIndex = 22;
+            label5.Text = "Dmin";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(477, 18);
+            label6.Name = "label6";
+            label6.Size = new Size(37, 15);
+            label6.TabIndex = 21;
+            label6.Text = "Dmax";
+            // 
+            // textBox6
+            // 
+            textBox6.Enabled = false;
+            textBox6.Location = new Point(643, 15);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(64, 23);
+            textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            textBox7.Enabled = false;
+            textBox7.Location = new Point(520, 15);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(64, 23);
+            textBox7.TabIndex = 19;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(602, 47);
+            label7.Name = "label7";
+            label7.Size = new Size(35, 15);
+            label7.TabIndex = 26;
+            label7.Text = "dmin";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(478, 47);
+            label8.Name = "label8";
+            label8.Size = new Size(36, 15);
+            label8.TabIndex = 25;
+            label8.Text = "dmax";
+            // 
+            // textBox8
+            // 
+            textBox8.Enabled = false;
+            textBox8.Location = new Point(643, 44);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(64, 23);
+            textBox8.TabIndex = 24;
+            // 
+            // textBox9
+            // 
+            textBox9.Enabled = false;
+            textBox9.Location = new Point(520, 44);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(64, 23);
+            textBox9.TabIndex = 23;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(619, 76);
+            label9.Name = "label9";
+            label9.Size = new Size(18, 15);
+            label9.TabIndex = 30;
+            label9.Text = "td";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(491, 76);
+            label10.Name = "label10";
+            label10.Size = new Size(22, 15);
+            label10.TabIndex = 29;
+            label10.Text = "TD";
+            // 
+            // textBox10
+            // 
+            textBox10.Enabled = false;
+            textBox10.Location = new Point(643, 73);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(64, 23);
+            textBox10.TabIndex = 28;
+            // 
+            // textBox11
+            // 
+            textBox11.Enabled = false;
+            textBox11.Location = new Point(520, 73);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(64, 23);
+            textBox11.TabIndex = 27;
+            // 
+            // textBox12
+            // 
+            textBox12.Enabled = false;
+            textBox12.Location = new Point(520, 114);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(187, 23);
+            textBox12.TabIndex = 31;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(439, 117);
+            label11.Name = "label11";
+            label11.Size = new Size(75, 15);
+            label11.TabIndex = 32;
+            label11.Text = "Вид посадки";
+            // 
+            // textBox13
+            // 
+            textBox13.Enabled = false;
+            textBox13.Location = new Point(520, 143);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(64, 23);
+            textBox13.TabIndex = 33;
+            // 
+            // textBox14
+            // 
+            textBox14.Enabled = false;
+            textBox14.Location = new Point(643, 146);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(64, 23);
+            textBox14.TabIndex = 34;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(487, 146);
+            label12.Name = "label12";
+            label12.Size = new Size(27, 15);
+            label12.TabIndex = 35;
+            label12.Text = "S/N";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(610, 149);
+            label13.Name = "label13";
+            label13.Size = new Size(27, 15);
+            label13.TabIndex = 36;
+            label13.Text = "S/N";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 15F);
+            label14.Location = new Point(12, 65);
+            label14.Name = "label14";
+            label14.Size = new Size(29, 28);
+            label14.TabIndex = 37;
+            label14.Text = "⌀";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(701, 458);
-            Controls.Add(button7);
-            Controls.Add(pictureBox1);
-            Controls.Add(button4);
-            Controls.Add(button5);
-            Controls.Add(button6);
-            Controls.Add(button3);
+            ClientSize = new Size(801, 597);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(label11);
+            Controls.Add(textBox12);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(textBox10);
+            Controls.Add(textBox11);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(textBox8);
+            Controls.Add(textBox9);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(textBox7);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
             Controls.Add(button2);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Расчет допусков и посадок";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
         private PictureBox pictureBox1;
-        private Button button7;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private Label label11;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private Label label12;
+        private Label label13;
+        private Label label14;
     }
 }
